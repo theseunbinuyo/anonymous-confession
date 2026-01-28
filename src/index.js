faade151f48a0d0976fcba82faf22a1b9ff8efb1
theseunbinuyo <beekonceptmarketinghub@gmai.com>
2026-01-28 15:29:39 +0100

    localhost fixed

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        ""
+        "https://anonymous-confession-three.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
